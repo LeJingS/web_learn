f42dd530986672b64d038774ad1436a5205146cb
LeJingS <lejings@qq.com>
2025-07-13 18:00:04 +0800

    Auto commit by script on 2025年 07月 13日 星期日 18:00:00 CST

--- 尚硅谷TypeScript学习/day01/day01.ts
+++ 尚硅谷TypeScript学习/day01/day01.ts
@@ -3,7 +3,7 @@
  * @Author: LeJingS
  * @Date: 2025-07-13 16:42:13
  * @LastEditors: LeJingS
- * @LastEditTime: 2025-07-13 16:42:14
+ * @LastEditTime: 2025-07-13 17:57:45
  * Copyright: 2025 xxxTech CO.,LTD. All Rights Reserved.
  * @Descripttion: ts学习开始
  */
